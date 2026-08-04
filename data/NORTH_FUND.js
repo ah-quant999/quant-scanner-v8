ba2b07efe0b09887d116d926c2a276e1854c8c08
ah-quant999 <2814546@qq.com>
2026-08-04 08:56:17 +0800

    deploy v8 lightweight

--- data/NORTH_FUND.js
+++ data/NORTH_FUND.js
@@ -1,1 +1,1 @@
-window.NORTH_FUND = {"stopped":true,"note":"港交所 2024-05 后停止披露北向 top_buy，无实时数据","update_time":"2026-08-04 08:52:23"};
+window.NORTH_FUND = {"stopped":true,"note":"港交所 2024-05 后停止披露北向 top_buy，无实时数据","update_time":"2026-08-04 08:55:31"};
