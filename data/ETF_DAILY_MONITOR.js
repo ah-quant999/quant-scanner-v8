da525d436efd40134db335818b3d3c1404897a7c
ah-quant999 <2814546@qq.com>
2026-08-07 09:10:12 +0800

    deploy v8 lightweight

--- data/ETF_DAILY_MONITOR.js
+++ data/ETF_DAILY_MONITOR.js
@@ -1,1 +1,1 @@
-window.ETF_DAILY_MONITOR = {"no_data":true,"note":"盘前无主力净流入数据，盘后 T+1 自动更新","update_time":"2026-08-07 08:54:25","build_time":"2026-08-07 08:56:37"};
+window.ETF_DAILY_MONITOR = {"no_data":true,"note":"盘前无主力净流入数据，盘后 T+1 自动更新","update_time":"2026-08-07 08:54:25","build_time":"2026-08-07 09:08:57"};
