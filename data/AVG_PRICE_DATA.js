60db9910fe1cda3da13cb8daaa8e434b874284b8
ah-quant999 <2814546@qq.com>
2026-08-08 09:21:44 +0800

    fix(v8): 三重入选历史改为5-6列网格展示；空卡片增加跟踪起始提示；同步数据

--- data/AVG_PRICE_DATA.js
+++ data/AVG_PRICE_DATA.js
@@ -1,1 +1,1 @@
-window.AVG_PRICE_DATA = {"update_time":"2026-08-08 09:10:11","build_time":"2026-08-08 09:11:38"};
+window.AVG_PRICE_DATA = {"update_time":"2026-08-08 09:10:11","build_time":"2026-08-08 09:21:28"};
