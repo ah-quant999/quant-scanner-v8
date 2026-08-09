1fd330b253dfa66d53098fcc2d6f3052a506bfaf
ah-quant999 <2814546@qq.com>
2026-08-09 23:06:32 +0800

    chore(build): rebase 后重建 data/*.js

--- data/AVG_PRICE_DATA.js
+++ data/AVG_PRICE_DATA.js
@@ -1,1 +1,1 @@
-window.AVG_PRICE_DATA = {"update_time":"2026-08-09 11:11:05","republish_time":"2026-08-09 23:04:59"};
+window.AVG_PRICE_DATA = {"update_time":"2026-08-09 11:11:05","republish_time":"2026-08-09 23:06:15"};
