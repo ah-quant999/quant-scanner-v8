cba4efd850c8b83c816df7c2dbe77d0022e4307a
ah-quant999 <2814546@qq.com>
2026-08-12 16:31:04 +0800

    data: 自动提交 60m 共振数据 FOUR_VOLUME_60M.js

--- data/FOUR_VOLUME_60M.js
+++ data/FOUR_VOLUME_60M.js
@@ -1,5 +1,5 @@
 window.FOUR_VOLUME_60M={
- "update_time": "2026-08-12 16:09:16",
+ "update_time": "2026-08-12 16:31:03",
  "total": 2,
  "period": "60m",
  "description": "四量终极 60分钟共振信号（加分因子，独立于日线版）",
