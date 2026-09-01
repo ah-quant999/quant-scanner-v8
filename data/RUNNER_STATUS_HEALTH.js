bab3c5afae0412b3e36690262296261914d02649
九宝量化 <2814546@qq.com>
2026-09-01 12:00:53 +0800

    清理生命周期子页遗留：修正陈旧注释 + 删除未引用的 __restoreStock

--- data/RUNNER_STATUS_HEALTH.js
+++
@@ -1,1 +0,0 @@
-window.RUNNER_STATUS_HEALTH = {"runner":"lemoncat-cn","state":"offline","checked_at_utc":"2026-08-31T19:16:27Z","checked_at_bj":"2026-09-01 03:16:27","update_time":"2026-09-01 03:16:27","source":"runner_health_alert.yml"};
