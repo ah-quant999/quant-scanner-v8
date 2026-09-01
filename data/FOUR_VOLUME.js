a3e03fe70fd809fce26020853aab779a8ca28c4e
ah-quant999 <2814546@qq.com>
2026-09-02 05:25:15 +0800

    data: 2026-09-02 盘后全量选股数据（43 ok / 3 非核心未产出），运维页转绿

--- data/FOUR_VOLUME.js
+++ data/FOUR_VOLUME.js
@@ -1,35 +1,7 @@
 window.FOUR_VOLUME={
- "update_time": "2026-09-01 21:03:28",
- "total": 2,
+ "update_time": "2026-09-02 04:44:06",
+ "total": 1,
  "stocks": [
-  {
-   "code": "002131",
-   "name": "利欧股份",
-   "market": "sz",
-   "board_label": "主板",
-   "close": 5.1,
-   "pct_chg": 7.37,
-   "turnover_rate": 17.48,
-   "mv_yi": 0,
-   "fund_type": "混合",
-   "components": {
-    "游资点火": true,
-    "机构托底": true,
-    "广度翻多": true,
-    "主力动量翻多": true,
-    "机构金叉": true,
-    "散户金叉": false,
-    "主力金叉": true
-   },
-   "yzc": true,
-   "jg": true,
-   "xc": true,
-   "four": true,
-   "qd": true,
-   "reason": "游资点火(YZC) + 机构托底(JG) + 广度翻多(GB1≥0) + 主力动量翻多(V6≥0) + 机构金叉 + 主力金叉",
-   "signal_date": "2026-09-01",
-   "enter_date": "2026-09-01"
-  },
   {
    "code": "300418",
    "name": "昆仑万维",
@@ -37,7 +9,7 @@ window.FOUR_VOLUME={
    "board_label": "创业板",
    "close": 45.94,
    "pct_chg": 4.43,
-   "turnover_rate": 12.4,
+   "turnover_rate": 0,
    "mv_yi": 0,
    "fund_type": "混合",
    "components": {
