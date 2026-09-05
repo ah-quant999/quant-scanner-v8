f58986e3712323b7ef08c420093a0131f054dc74
ah-quant999 <2814546@qq.com>
2026-09-05 17:31:56 +0800

    deploy v8 lightweight

--- data/STOCK_MOMENTUM_STATE.js
+++ data/STOCK_MOMENTUM_STATE.js
@@ -1,9 +1,16 @@
 window.STOCK_MOMENTUM_STATE = (function() {
   var data = {
+<<<<<<< Updated upstream
  "update_time": "2026-09-05 17:29",
  "generated": "2026-09-05 17:29",
  "meta": {
   "generated": "2026-09-05 17:29",
+=======
+ "update_time": "2026-09-05 17:30",
+ "generated": "2026-09-05 17:30",
+ "meta": {
+  "generated": "2026-09-05 17:30",
+>>>>>>> Stashed changes
   "source": "h_reverse_upgraded_breakout(脱离PDF OCR)",
   "total_days": 4,
   "days_with_consensus": 0,
